6178c3f9419733bf983eac2b92f8a15f42a250d9
lennonalberto <lennonalberto@gmail.com>
2014-05-12 17:20:06 -0700

    Implentado no display para mostrar o gráfico do aluno selecionado.

--- visualizeitor-rails/app/assets/javascripts/displays.js
+++ visualizeitor-rails/app/assets/javascripts/displays.js
@@ -20,6 +20,9 @@ function registerCallbacks()
     window.scrollTo(0,document.body.scrollHeight);
     gridContentClick(this.id);
   });
+  $(".tabSelector").click(function (){
+    tabChange(this.id);
+  });
 }
 
 function didClickButtonSearchGRR()
@@ -32,7 +35,8 @@ function didClickButtonSearchGRR()
       data: {GRR: enteredGRR},
       success: function (data) {
         studentJson = data;
-        didDownloadStudent(data);        
+        didDownloadStudent(data); 
+        getChartData();       
       }
     });
 
@@ -343,5 +347,119 @@ function updateCourseInfo (contentId){
     }
     else $('#courseOldInfo').css({"display": "none" });
   }
+
+}
+
+
+function getChartData(){
+
+    var studentId = studentJson.id;
+
+    $.ajax({
+            url: '/students/' + studentId + '/enrollments/ira_chart.json',
+            type: 'get',
+            data: {},
+            success: function (data) {
+              loadChart(data);
+            }
+        });
+}
+
+
+function loadChart(data)
+{
+  gradeFloat = []
+  for (i = 0; i < data.grade_average.length; i++) {
+    gradeFloat[i] = parseFloat(data.grade_average[i]);
+  }
+        $('#chartContainer').highcharts({
+            exporting: {
+                enabled: true
+            },
+            credits: {
+                enabled: false
+            },
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Notas e Quantidade de Disciplinas por Semestre'
+            },
+            xAxis: [{
+                categories: data.years
+            }],
+            yAxis: [{ // Primary yAxis
+              min: 0,
+              max: 100,
+                labels: {
+                    format: '{value}',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                title: {
+                    text: 'Nota',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                }
+            }, { // Secondary yAxis
+                title: {
+
+                    text: 'Quantidade de Disciplinas',
+                    style: {
+                        color: '#353C42'
+                    }
+                },
+                labels: {
+                    format: '{value}',
+                    style: {
+                        color: '#458F8C'
+                    }
+                },
+                opposite: true
+            }],
+            tooltip: {
+                shared: true
+            },
+            
+            series: [{
+              color: '#458F8C',
+                name: 'Quantidade de Disciplinas',
+                type: 'column',
+                yAxis: 1,
+                data: data.total_enrollments,
+                tooltip: {
+                }
+    
+            }, {
+                name: 'Nota',
+                color: '#353C42',
+                marker: {
+                lineWidth: 2,
+                lineColor: '#353C42',
+                fillColor: '#BCD3E7'
+              },
+                data: gradeFloat,
+                lineWidth: 2,
+
+            }]
+        });
+}
+
+function tabChange(tabId){
+
+  $('.tabSelector').removeClass('selectedTab');
+  $('#'+tabId).addClass('selectedTab');
+
+  if(tabId == "tabGrid"){
+    $('#chartTab').hide();
+    $('#gridTab').show();
+  }
+  else if (tabId == "tabChart"){
+    $('#chartTab').show();
+    $('#gridTab').hide();
+  }
+
 
 }
