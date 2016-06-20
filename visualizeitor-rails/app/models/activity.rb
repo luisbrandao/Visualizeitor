063ac04a5b07f8c950a8f79f698ef6ea31d00d5e
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2016-06-20 13:31:54 -0300

    Melhoria na visualização de horas válidas de atividades

--- visualizeitor-rails/app/models/activity.rb
+++ visualizeitor-rails/app/models/activity.rb
@@ -1,19 +1,22 @@
 class Activity < ActiveRecord::Base
   has_many :acdocs, dependent: :destroy, autosave: true
+  belongs_to :acform
+  belongs_to :category
+
 
   accepts_nested_attributes_for :acdocs,
                                 reject_if: proc { |attributes| attributes['descr'].blank?},
                                 allow_destroy: true
 
 
-  belongs_to :acform
-  belongs_to :category
 
   validates :hours, :acform_id, :category_id, presence: true
 
   def valid_hours
-    return hours
+    if self.hours >= self.category.hardlimit
+      return self.category.hardlimit
+    else
+      return self.hours
+    end
   end
-
-
 end
